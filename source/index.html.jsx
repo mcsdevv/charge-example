5c014099cea5d39507133323450b56369bf5e314
msweeneydev <mail@msweeneydev.com>
2020-04-15 16:58:04 +0100

    test

--- source/index.html.jsx
+++ source/index.html.jsx
@@ -3,7 +3,7 @@ import About from "./pages/about.html.mdx";
 export default () => {
   return (
     <>
-      <h1>Charge.js Deployed on ZEIT Now</h1>
+      <h1>Charge.js Deployed on ZEIT Now - new test</h1>
       <About />
     </>
   );
